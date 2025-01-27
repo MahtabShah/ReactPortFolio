59d37c91fa5db5c1adb84c3b5d0a6bbf3f9e459a
= <cre8.hystory@gmail.com>
2025-01-27 15:09:58 +0530

    Initial commit of React + Vite project

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+// import img from "./IMG-20230917-WA0002.jpg";
+import Background from "./background";
+
+const App = () => {
+  return (
+    <>
+      <Background></Background>
+    </>
+  );
+};
+
+export default App;
