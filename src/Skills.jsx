2007886b57798c6aacfcf07099e4e2478963389e
= <cre8.hystory@gmail.com>
2025-03-02 16:24:21 +0530

    Initial commit

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -64,7 +64,7 @@ export function SkillCmponent({ ht, wd, nm, svg }) {
     <>
       <div className="skill" style={{ height: ht, width: wd }}>
         <div className="divSvg">
-          <object type="image/svg+xml" src={`./src/asset/${svg}.svg`} />
+          <embed type="image/svg+xml" src={`./src/asset/${svg}.svg`} />
         </div>
         <span>{nm}</span>
       </div>
