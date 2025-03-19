79e5c896e4af0addcc0490c44f35b7414a79269e
= <cre8.hystory@gmail.com>
2025-03-19 17:03:57 +0530

    Initial commit

--- src/background.jsx
+++ src/background.jsx
@@ -80,7 +80,6 @@ export function Page() {
           </div>
           <div className="ph">
             <div className="photo"></div>
-            {/* <div className="cr"></div> */}
           </div>
         </div>
 
