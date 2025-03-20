6c608c14c857b6a8082f642eb8f0211c49635902
= <cre8.hystory@gmail.com>
2025-03-20 13:59:43 +0530

    Initial commit

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,6 +3,8 @@ import "./Footer.css";
 import "./Achive.css";
 import img1 from "./WhatsApp Image 2025-02-24 at 10.17.48_6013a730.jpg";
 import img2 from "./AWhatsApp Image 2025-02-24 at 10.22.17_04ea43b1.jpg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 import { Shape } from "./Skills";
 
@@ -10,71 +12,20 @@ export default function FooTer() {
   const img = [img1, img2];
   return (
     <>
-      {/* <div className="BgParent">
-        <div className="Achived-Section">
-          <Cirtificate images={img[0]} />
-          <Cirtificate images={img[1]} />
-        </div>
-      </div> */}
-      <div className="achiveBg">
-        <Shape
-          style={{
-            left: "-125px",
-            bottom: "25px",
-            filter: "blur(3px)",
-            width: "200px",
-            height: "200px",
-          }}
-        ></Shape>
-
-        <Shape
-          style={{
-            right: "calc(40vw - 20px)",
-            bottom: "calc(50vh - 3vw)",
-            filter: "blur(3px)",
-            width: "calc(min(10vw, 60px))",
-            height: "calc(min(10vw, 60px))",
-            background: `repeating-linear-gradient(
-              120deg,
-              rgb(142, 3, 241),
-rgba(254, 157, 11, 0.95)
-            )`,
-          }}
-        ></Shape>
-
-        <Shape
-          style={{
-            right: "calc(max(-40vw, -420px))",
-            bottom: "calc(45vh - 20vw)",
-            filter: "blur(3px)",
-            width: "calc(min(50vw, 600px))",
-            height: "calc(min(50vw, 600px))",
-            background: `repeating-linear-gradient(
-              170deg,
-              rgb(142, 3, 241),
-rgba(254, 157, 11, 0.95)
-            )`,
-          }}
-        ></Shape>
-
-        <div className="abtCate">
+      <div className="bg-2 d-flex flex-column">
+        <div className="div-crt m-3 p-2 d-flex flex-column flex-grow-1">
           <span className="h2div hdng">CIRTIFICATION</span>
-          <p className="abtp">
+          <p className="p-2 text-center">
             The course which i compleated.
             <br /> 1. C++ DSA cirtification from APNA COLLAGE.
             <br />
             2. Web Development Workshop from AADI Foundation.
           </p>
         </div>
-        <div className="Achmph">
-          <div className="f1c">
-            <Cirtificate images={img[0]} />
-          </div>
-          <div className="f2c">
-            <Cirtificate images={img[1]} />
-          </div>
+        <div className="m-3 p-3 d-flex gap-3 cf-2 justify-content-around align-items-center flex-row">
+          <Cirtificate images={img[0]} />
+          <Cirtificate images={img[1]} />
         </div>
-        <div className="Rcft"></div>
       </div>
     </>
   );
@@ -82,8 +33,8 @@ rgba(254, 157, 11, 0.95)
 
 function Cirtificate({ images }) {
   return (
-    <div className="Cirtificate">
-      <img src={images} alt="" className="img" />
+    <div className="Cirtificate d-flex border flex-grow-1 w-100">
+      <img src={images} alt="" className="img w-100 rounded-1" />
     </div>
   );
 }
