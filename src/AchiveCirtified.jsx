6b55e7f56a4c3de92e04a27687b4c1a17fc2e1cf
Mahtab Shah <mahtab.shah916@gmail.com>
2025-04-03 15:09:38 +0530

    Update AchiveCirtified.jsx

--- src/AchiveCirtified.jsx
+++ src/AchiveCirtified.jsx
@@ -1,14 +1,16 @@
 import "./Achive.css";
 import img1 from "./WhatsApp Image 2025-02-24 at 10.17.48_6013a730.jpg";
 import img2 from "./AWhatsApp Image 2025-02-24 at 10.22.17_04ea43b1.jpg";
+import img3 from "./7a850459-740a-40de-9b8f-d27e2d2f5e0c.jpg";
 
 export default function Achive() {
-  const img = [img1, img2];
+  const img = [img1, img2 , img3];
   return (
     <>
       <div className="Achived-Section">
         <Cirtificate images={img[0]} style={{ alignSelf: "end" }} />
         <Cirtificate images={img[1]} />
+        <Cirtificate images={img[2]} />
       </div>
 
       <div className="achiveBg"></div>
