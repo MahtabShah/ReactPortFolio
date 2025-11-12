0ef53bc5384e0d999abb49db37c5310ecb1baa7b
MahtabShah <mahtab.shah916@gmail.com>
2025-11-12 21:28:04 +0530

    updated

--- src/SocialIcon.jsx
+++ src/SocialIcon.jsx
@@ -7,8 +7,8 @@ const SocialIcons = () => {
     { name: "facebook", link: "#" },
     { name: "twitter", link: "#" },
     { name: "github", link: "https://github.com/MahtabShah/" },
-    { name: "linkedin", link: "#" },
-    { name: "instagram", link: "#" },
+    { name: "linkedin", link: "linkedin/mahtab-shah-916" },
+    { name: "instagram", link: "" },
     { name: "google-plus", link: "#" },
   ];
 
